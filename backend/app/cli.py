bb836351d2b399c9a003de7e2bfdfc84bbb1b930
Nuno Campos <nuno@boringbits.io>
2024-03-23 10:42:27 -0700

    Update cli

--- backend/app/cli.py
+++ backend/app/cli.py
@@ -1,7 +1,8 @@
 import asyncio
-import importlib
+import importlib.util
 import os
 import pathlib
+import sys
 from typing import Optional
 
 import asyncpg
@@ -15,15 +16,19 @@ from app.lib.checkpoint import PostgresCheckpoint
 from app.lib.lifespan import connect
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.argument(
     "path",
     required=False,
-    type=str,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True),
 )
-def start(path: Optional[str]):
-    """Simple program that greets NAME for a total of COUNT times."""
-
+@click.option("--debug", is_flag=True, help="Print debug information", default=False)
+def start(path: Optional[str], debug: bool):
     os.environ.update(
         {
             "POSTGRES_PORT": "5433",
@@ -40,14 +45,18 @@ def start(path: Optional[str]):
         print(f"Starting LangGraph Studio with graph at path: {path}")
 
         try:
-            module = importlib.import_module(path)
+            module_name = "graph_module"
+            spec = importlib.util.spec_from_file_location(module_name, path)
+            module = importlib.util.module_from_spec(spec)
+            sys.modules[module_name] = module
+            spec.loader.exec_module(module)
         except ImportError as e:
-            print(f"Could not import python module at path: {path} {e}")
+            print(f"Could not import python module at path: {path} debug: {e}")
             return
         try:
             graph = module.graph
-        except AttributeError:
-            print(f"Could not find `graph` in module at path: {path}")
+        except AttributeError as e:
+            print(f"Could not find `graph` in module at path: {path}. debug: {e}")
             return
         if isinstance(graph, Pregel):
             pass
@@ -64,7 +73,7 @@ def start(path: Optional[str]):
     compose_spec = pathlib.Path(__file__).parent / "../../docker-compose-cli.yml"
 
     async def serve_once_ready():
-        while True:
+        for _ in range(120):
             conn: Optional[asyncpg.Connection] = None
             try:
                 print("Waiting for database to be ready...")
@@ -72,18 +81,26 @@ def start(path: Optional[str]):
                 await conn.execute("select * from assistant limit 1")
                 break
             except Exception as exc:
-                print(f"Database not ready, retrying... {exc}")
+                if debug:
+                    print(f"Database not ready, retrying... {exc}")
                 if conn:
                     await conn.close()
-                await asyncio.sleep(0.5)
+                await asyncio.sleep(1)
+        else:
+            raise Exception("Database not ready after 120 seconds. Exiting...")
 
         await server.serve()
 
     async def run_both():
-        await asyncio.gather(
-            exec_cmd("docker", "compose", "-f", str(compose_spec.resolve()), "up"),
-            serve_once_ready(),
-        )
+        try:
+            await asyncio.gather(
+                exec_cmd("docker", "compose", "-f", str(compose_spec.resolve()), "up"),
+                serve_once_ready(),
+            )
+        finally:
+            await exec_cmd(
+                "docker", "compose", "-f", str(compose_spec.resolve()), "down"
+            )
 
     server.config.setup_event_loop()
     asyncio.run(run_both())
@@ -93,10 +110,8 @@ async def exec_cmd(cmd: str, *args: str):
     proc = await asyncio.create_subprocess_exec(cmd, *args)
     await proc.communicate()
 
-    if proc.returncode != 0 and proc.returncode != 130:
-        print(proc.returncode)
+    if (
+        proc.returncode != 0  # success
+        and proc.returncode != 130  # user interrupt
+    ):
         raise Exception(f"Command failed: {cmd} {' '.join(args)}")
-
-
-if __name__ == "__main__":
-    start()
